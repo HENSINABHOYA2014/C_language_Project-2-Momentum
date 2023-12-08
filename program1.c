eaec4c0f424ac3c69ecd6e0d95c64957a9732895
Hensi Nabhoya <hensinabhoya123@gmail.com>
2023-12-08 16:29:45 +0530

    first commit

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int a;
+	printf("Enter Eny Number:");
+	scanf("%d",&a);
+	
+	
+	(a%2==0) ? printf("This Number is Even Number.\n") : printf("This Number is Odd Number");
+    
+ 
+}
